fe468fd5a48564eeba1e9911b2e559b81c222dd4
Andrej-Dunda <dundaandrej@seznam.cz>
2024-04-26 17:55:15 +0200

    added modal

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <button className='new-tournament'>Nový turnaj</button>
+      <button className='new-tournament dark'>Nový turnaj</button>
     </div>
   )
 }
